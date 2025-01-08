de14859a7ccf4e29522f28060a761c7f4b2f80ae
NafisFuad106 <107258321+NafisFuadShahid@users.noreply.github.com>
2025-01-08 20:20:46 +0600

    videocall

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import { AgoraRTCProvider } from 'agora-rtc-react';
+// Create Agora RTC client
+const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+    <AgoraRTCProvider client={client}>
+      <App />
+    </AgoraRTCProvider>
+ 
 );
